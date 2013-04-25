b922cfa37ded661810e1ed213b5a72d696ebc85d
Miguel Senquiz <miguel@ghostly.com>
2013-04-25 15:18:44 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
-apt-get install mysql-client-core-5.5
-rm -rf /var/www
-ln -fs /vagrant /var/www
+###MAKE SURE THE CURRENT USER IS ROOT###
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+aptitude update;
+aptitude install -y nginx php5-cli php5-curl php5-sqlite php5-mysql php5 php5-fpm mysql-server curl;
